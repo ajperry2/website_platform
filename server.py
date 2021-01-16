6f6b299f92ac89cbe7d400baaae439279619a220
Alan Perry <ajperry2@usfca.edu>
2021-01-16 03:26:42 -0800

    Missing import added

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 #app.py
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World2!'
 @app.route('/say-my-name')
 def hello_world():
     return 'Hello, ' + request.args.get('name')
